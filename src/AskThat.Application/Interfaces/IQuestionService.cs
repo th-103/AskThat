ef3cd77f2358337d66dd8244cf0114f61afaa9dd
th153 <localhost>
2025-07-04 18:56:30 +0300

    blank services

---
+++ src/AskThat.Application/Interfaces/IQuestionService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AskThat.Application.Interfaces
+{
+    public interface IQuestionService
+    {
+        
+    }
+}
